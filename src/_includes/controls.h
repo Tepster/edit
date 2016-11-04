cf63fd8a39822ecfe0069d06a67e01f7c34a9198
Martin Heralecký <heralecky.martin@gmail.com>
2016-11-04 20:55:48 +0100

    remove unnecessary comments in controls.h

--- src/_includes/controls.h
+++ src/_includes/controls.h
@@ -19,30 +19,22 @@ class _t::controls::label : public QLabel
     Q_OBJECT
 
     /**
-     * Event for mouse press.
-     *
      * @param QMouseEvent * event
      */
     void mousePressEvent(QMouseEvent *event);
 
     /**
-     * Event for mouse move.
-     *
      * @param QMouseEvent * event
      */
     void mouseMoveEvent(QMouseEvent *event);
 
 signals:
     /**
-     * Signal for mouse press.
-     *
      * @param QMouseEvent * event
      */
     void mouse_pressed(QMouseEvent *event);
 
     /**
-     * Signal for mouse move.
-     *
      * @param QMouseEvent * event
      */
     void mouse_moved(QMouseEvent *event);
